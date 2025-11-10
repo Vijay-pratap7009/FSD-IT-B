f02069fbf36626f54cc0c13a1a9cc22dd2876998
Vijay-pratap7009 <itsmenerdgeek.@gmail.com>
2025-11-10 13:06:29 +0530

    added Shopping App

---
+++ Shopping App/client/src/Pages/Admindashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Componenet/Header'
+import Footer from '../Componenet/Footer'
+
+const Admindashboard = () => {
+  return (
+    <div>
+      <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Admindashboard
---
+++ Shopping App/client/src/Pages/UserDashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Componenet/Header'
+import Footer from '../Componenet/Footer'
+import UserNavbar from '../Componenet/UserNavbar'
+
+const UserDashboard = () => {
+  return (
+    <div>
+      <Header/>
+      <UserNavbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default UserDashboard
